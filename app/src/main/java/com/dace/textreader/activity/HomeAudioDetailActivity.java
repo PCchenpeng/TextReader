403a47a8ab55f2f8a5e69232e606e4b7e7c4c5a9
cp3 <751899906@qq.com>
2019-03-22 12:02:56 +0800

    測試2

--- app/src/main/java/com/dace/textreader/activity/HomeAudioDetailActivity.java
+++ app/src/main/java/com/dace/textreader/activity/HomeAudioDetailActivity.java
@@ -14,8 +14,6 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
